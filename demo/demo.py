2a1fea2a148df75cb6376b0679ea6e8888b2f0ce
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-05-30 23:33:26 -0400

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -43,7 +43,7 @@ for option in options:
 input_variable = os.environ['INPUT_STORE']
 
 
-input_variable2 = os.environ['INPUT_STORE2']
+input_variable2 = ""
 input_variable3 = os.environ['INPUT_STORE3']
 input_variable4 = os.environ['INPUT_STORE4']
 

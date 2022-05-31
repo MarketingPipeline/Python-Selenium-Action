56592785a5ff42c63aab6ee01711e282f2ecbf4f
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-05-31 01:09:40 -0400

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -35,3 +35,6 @@ driver = webdriver.Chrome()
 
 driver.get('http://github.com')
 print(driver.title)
+with open('./GitHub_Action_Results.txt', 'w') as f:
+    f.write(f"This was written with a GitHub action {driver.title}")
+

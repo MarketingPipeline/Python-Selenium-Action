2cd488afe0f5643d0ce15adcb73a0ca65b4b6532
oror-sine <100359826+oror-sine@users.noreply.github.com>
2024-03-31 01:18:07 +0900

    Update Selenium-Template.py
    가상화면 없으면 깃업액션에서 에러나는지 확인용

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
-display.start()
+# from pyvirtualdisplay import Display
+# display = Display(visible=0, size=(800, 800))  
+# display.start()
 
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
                                       # and if it doesn't exist, download it automatically,

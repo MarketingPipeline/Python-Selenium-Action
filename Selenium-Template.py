f62eaabf102f7e607399fbe5e4db74eccebf7134
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-05-27 01:01:19 -0400

    Fixed Chrome Options 🔧
    Enabled some flags to fix issues with this action.

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -10,23 +10,23 @@ chromedriver_autoinstaller.install()  # Check if the current version of chromedr
                                       # and if it doesn't exist, download it automatically,
                                       # then add chromedriver to path
 
-chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
+chrome_options = webdriver.ChromeOptions()
+# Add your options as needed
 options = [
-  # Define window size here
-   "--window-size=1200,1200",
+   # Define window size here
+    "--window-size=1200,1200",
     "--ignore-certificate-errors"
  
     #"--headless",
-    #"--disable-gpu",
     #"--window-size=1920,1200",
     #"--ignore-certificate-errors",
     #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
+    # These flags BELOW are recommended for stability when running Chrome in headless or containerized environments (such as GitHub Actions).
+    "--disable-gpu",
+    "--no-sandbox",
+    "--disable-dev-shm-usage",
+    '--remote-debugging-port=9222'
 ]
-
 for option in options:
     chrome_options.add_argument(option)
 
@@ -37,4 +37,3 @@ driver.get('http://github.com')
 print(driver.title)
 with open('./GitHub_Action_Results.txt', 'w') as f:
     f.write(f"This was written with a GitHub action {driver.title}")
-
